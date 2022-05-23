a95b48ea592f72036ea5fbd1c1a98ce5ca46df5a
Natalia Banasiewicz <natalia.banasiewicz@wp.pl>
2022-05-23 15:39:21 +0200

    Add code first exercise code

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+
+namespace CodeFirstNewDatabaseSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                // Create and save a new Blog
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+
+                //var blog2 = new Blog { Name = "test" };
+                //db.Blogs.Add(blog2);
+
+                db.SaveChanges();
+
+                // Display all Blogs from the database
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+
+        public class User
+        {
+            [Key]
+            public string Username { get; set; }
+            public string DisplayName { get; set; }
+        }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Name { get; set; }
+
+            public string Url { get; set; }
+            public virtual List<Post> Posts { get; set; }
+        }
+
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            public int BlogId { get; set; }
+            public virtual Blog Blog { get; set; }
+        }
+
+        public class BloggingContext : DbContext
+        {
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
+            public DbSet<User> Users { get; set; }
+
+            // Fluent API
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                //Changing column name from DisplayName to display_name
+                modelBuilder.Entity<User>()
+                    .Property(u => u.DisplayName)
+                    .HasColumnName("display_name");
+
+                //Changing Database table name to Metadata
+                modelBuilder.Entity<User>()
+                    .ToTable("Customers");
+
+                //Entity Framework will pluralise the name
+                //Remove the pluralisation of table names
+                //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            }
+        }
+    }
+}
